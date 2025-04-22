889a56dea4749c780ed01b2f8e5f0a4e3f43620e
midday2612 <midday2612@gmail.com>
2025-04-22 16:11:51 +0900

    KW-139/feat:Area, Building, EnterStatus 도메인 생성

--- src/main/java/com/doubleo/areaservice/domain/area/domain/Area.java
+++ src/main/java/com/doubleo/areaservice/domain/area/domain/Area.java
@@ -2,6 +2,8 @@ package com.doubleo.areaservice.domain.area.domain;
 
 import com.doubleo.areaservice.domain.common.model.BaseTimeEntity;
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,4 +15,17 @@ public class Area extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "area_id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "building_id", nullable = false)
+    private Building building;
+
+    @Column(name = "area_name", nullable = false)
+    private String areaName;
+
+    @Column(name = "area_category", length = 20)
+    private String category;
+
+    @OneToMany(mappedBy = "area")
+    private List<EnterStatus> enterStatuses = new ArrayList<>();
 }
