6dec5dfa70df7ec98170df33378bb10779aaad55
midday2612 <midday2612@gmail.com>
2025-04-28 17:32:32 +0900

    KW-184/feat: hospital entity unique 추가

--- src/main/java/com/doubleo/hospitalservice/domain/hospital/domain/Hospital.java
+++ src/main/java/com/doubleo/hospitalservice/domain/hospital/domain/Hospital.java
@@ -8,18 +8,25 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(
+        name = "hospital",
+        uniqueConstraints = {
+            @UniqueConstraint(
+                    name = "UniqueTenentId",
+                    columnNames = {"tenentId"})
+        })
 public class Hospital extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "hospital_id")
     private Long id;
 
-    @Column(name = "tenent_id")
+    @Column(name = "tenent_id", unique = true, nullable = false)
     private Long tenentId;
 
-    @Column(name = "hospital_name")
+    @Column(name = "hospital_name", nullable = false)
     private String hospitalName;
 
-    @Column(name = "hospital_policy")
+    @Column(name = "hospital_policy", nullable = false)
     private String hospitalPolicy;
 }
