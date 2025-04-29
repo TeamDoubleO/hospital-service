5f5165d0b8697ed4b0d41bf4d6bab0f3a30d4b2c
midday2612 <midday2612@gmail.com>
2025-04-29 15:14:49 +0900

    KW-185/feat: 예외 처리 코드 추가

--- src/main/java/com/doubleo/hospitalservice/domain/hospital/service/HospitalService.java
+++ src/main/java/com/doubleo/hospitalservice/domain/hospital/service/HospitalService.java
@@ -3,6 +3,8 @@ package com.doubleo.hospitalservice.domain.hospital.service;
 import com.doubleo.hospitalservice.domain.hospital.dto.response.HospitalDetailInfoDto;
 import com.doubleo.hospitalservice.domain.hospital.dto.response.HospitalInfoListDto;
 import com.doubleo.hospitalservice.domain.hospital.repository.HospitalRepository;
+import com.doubleo.hospitalservice.global.exception.CommonException;
+import com.doubleo.hospitalservice.global.exception.errorcode.HospitalErrorCode;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,20 @@ public class HospitalService {
     private final HospitalRepository hospitalRepository;
 
     public List<HospitalInfoListDto> getAllHospitals() {
-        return hospitalRepository.findAll().stream().map(HospitalInfoListDto::from).toList();
+        List<HospitalInfoListDto> hospitalList =
+                hospitalRepository.findAll().stream().map(HospitalInfoListDto::from).toList();
+
+        if (hospitalList.isEmpty()) {
+            throw new CommonException(HospitalErrorCode.HOSPITAL_LIST_EMPTY);
+        }
+
+        return hospitalList;
     }
 
     public HospitalDetailInfoDto getHospitalById(Long id) {
-        return HospitalDetailInfoDto.from(hospitalRepository.findById(id).get());
+        return hospitalRepository
+                .findById(id)
+                .map(HospitalDetailInfoDto::from)
+                .orElseThrow(() -> new CommonException(HospitalErrorCode.HOSPITAL_NOT_FOUND));
     }
 }
