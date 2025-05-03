8933554dc1eaf8bffe84e979b2429ae6070cf79c
midday2612 <midday2612@gmail.com>
2025-05-04 01:23:19 +0900

    KW-188/fix: controller 반환 타입 수정

--- src/main/java/com/doubleo/hospitalservice/domain/area/controller/AreaController.java
+++ src/main/java/com/doubleo/hospitalservice/domain/area/controller/AreaController.java
@@ -5,7 +5,6 @@ import com.doubleo.hospitalservice.domain.area.service.AreaService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +19,7 @@ public class AreaController {
 
     @GetMapping
     @Operation(summary = "Get Areas by Building ID", description = "건물 ID로 구역 리스트 조회")
-    public ResponseEntity<List<AreaInfoResponse>> areaListGetByBuildingId(
-            @RequestParam Long buildingId) {
-        List<AreaInfoResponse> areas = areaService.getAreaListByBuildingId(buildingId);
-        return ResponseEntity.ok(areas);
+    public List<AreaInfoResponse> areaListGetByBuildingId(@RequestParam Long buildingId) {
+        return areaService.getAreaListByBuildingId(buildingId);
     }
 }
