936fcb57a99548dfeb46f8c9f355acbe69798f6e
midday2612 <midday2612@gmail.com>
2025-05-08 16:42:33 +0900

    KW-306/fix: hoispital policy LocalDateTime으로 수정

--- src/main/java/com/doubleo/hospitalservice/domain/hospitalpolicy/domain/HospitalPolicy.java
+++ src/main/java/com/doubleo/hospitalservice/domain/hospitalpolicy/domain/HospitalPolicy.java
@@ -2,6 +2,7 @@ package com.doubleo.hospitalservice.domain.hospitalpolicy.domain;
 
 import com.doubleo.hospitalservice.domain.common.model.BaseEntity;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +17,12 @@ public class HospitalPolicy extends BaseEntity {
     private Long id;
 
     // 방문 예정 날짜 00:00 기준으로 N시간 전부터 신청 가능
-    // hospital_policy_reserve_offset = 10이면 전날 14:00 부터 신청(발급) 가능
-    @Column(name = "hospital_policy_reserve_offset")
-    private Long hospitalPolicyReserveOffset;
+    // reserveStartTime = 10:00이면 전날 14:00 부터 신청(발급) 가능
+    @Column(name = "hospital_policy_reserve_start_time")
+    private LocalDateTime reserveStartTime;
+
+    // 방문 예정 다음 날짜 00:00 기준으로 N시간 전까지 신청 가능
+    // reserveEndTime = 4:00이면 10이면 방문 당일 20:00 까지 신청(발급) 가능
+    @Column(name = "hospital_policy_reserve_end_time")
+    private LocalDateTime reserveEndTime;
 }
