f078bfe8a60ec0751295950628aa035e17b9a57b
midday2612 <midday2612@gmail.com>
2025-05-09 17:53:54 +0900

    KW-332/feat: 신청 가능 날짜 조회

--- src/main/java/com/doubleo/hospitalservice/domain/hospitalpolicy/controller/HospitalPolicyController.java
+++ src/main/java/com/doubleo/hospitalservice/domain/hospitalpolicy/controller/HospitalPolicyController.java
@@ -1,6 +1,7 @@
 package com.doubleo.hospitalservice.domain.hospitalpolicy.controller;
 
 import com.doubleo.hospitalservice.domain.hospitalpolicy.dto.request.HospitalPolicyInfoRequest;
+import com.doubleo.hospitalservice.domain.hospitalpolicy.dto.response.HospitalAvailableDateResponse;
 import com.doubleo.hospitalservice.domain.hospitalpolicy.dto.response.HospitalPolicyInfoResponse;
 import com.doubleo.hospitalservice.domain.hospitalpolicy.service.HospitalPolicyService;
 import jakarta.validation.Valid;
@@ -8,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/policies")
+@RequestMapping("policies")
 @RequiredArgsConstructor
 public class HospitalPolicyController {
 
@@ -22,5 +23,10 @@ public class HospitalPolicyController {
     @PatchMapping
     public void updateMyHospitalPolicy(@Valid @RequestBody HospitalPolicyInfoRequest request) {
         hospitalPolicyService.updatePolicyByTenantId(request);
+    }
+
+    @GetMapping("/available-dates")
+    public HospitalAvailableDateResponse getAvailableDates() {
+        return hospitalPolicyService.getAvailableDatesByTenantId();
     }
 }
