4edf374a51cda9bbbface12991e011b8cbd9f529
midday2612 <midday2612@gmail.com>
2025-05-14 12:10:29 +0900

    KW-375/feat: 전체 병원 목록 조회 - 공백 제거

--- src/main/java/com/doubleo/hospitalservice/domain/hospital/dto/response/HospitalInfoResponse.java
+++ src/main/java/com/doubleo/hospitalservice/domain/hospital/dto/response/HospitalInfoResponse.java
@@ -7,6 +7,7 @@ public record HospitalInfoResponse(
         @Schema(description = "병원 아이디", example = "1") Long hospitalId,
         @Schema(description = "병원 이름", example = "서울아산병원") String hospitalName) {
     public static HospitalInfoResponse from(Hospital hospital) {
-        return new HospitalInfoResponse(hospital.getId(), hospital.getHospitalName());
+        return new HospitalInfoResponse(
+                hospital.getId(), hospital.getHospitalName().replaceAll(" ", ""));
     }
 }
