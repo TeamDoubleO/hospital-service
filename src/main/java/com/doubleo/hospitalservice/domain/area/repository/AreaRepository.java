9045ecdc0d05bcf17804a36b025626b70140425f
coffeesigma <ljb22060@gmail.com>
2025-05-16 11:15:05 +0900

    KW-389/feat: 빌딩 이름 반환하는 gRPC 구현

--- src/main/java/com/doubleo/hospitalservice/domain/area/repository/AreaRepository.java
+++ src/main/java/com/doubleo/hospitalservice/domain/area/repository/AreaRepository.java
@@ -2,8 +2,11 @@ package com.doubleo.hospitalservice.domain.area.repository;
 
 import com.doubleo.hospitalservice.domain.area.domain.Area;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AreaRepository extends JpaRepository<Area, Long> {
     List<Area> findAllByBuilding_BuildingId(Long buildingId);
+
+    Optional<Area> findByTenantIdAndAreaCode(String tenantId, String areaCode);
 }
--- src/main/java/com/doubleo/hospitalservice/domain/building/repository/BuildingRepository.java
+++ src/main/java/com/doubleo/hospitalservice/domain/building/repository/BuildingRepository.java
@@ -2,10 +2,13 @@ package com.doubleo.hospitalservice.domain.building.repository;
 
 import com.doubleo.hospitalservice.domain.building.domain.Building;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, Long> {
     List<Building> findAllByTenantId(String tenantId);
+
+    Optional<Building> findByTenantIdAndBuildingCode(String tenantId, String buildingCode);
 }
