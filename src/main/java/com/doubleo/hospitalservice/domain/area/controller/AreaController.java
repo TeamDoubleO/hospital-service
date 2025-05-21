55f6c0ca1ed2e845568727aaa64b908f43227f14
Minji Son <109676875+midday2612@users.noreply.github.com>
2025-05-21 13:30:35 +0900

    [KW-409] Kw 409/refactor/get building (#27)
    * KW-409/refactor: 관리자가 병원 목록 조회
    
    * KW-409/feat: 컨트롤러 url 앞에 hospitals 추가
    
    * KW-409/chore:건물 조회 메서드 이름 변경
    
    * KW-409/chore: 스웨거 설명 수정
    
    * KW-409/refactor: 건물별 구역 조회 url 변경

--- src/main/java/com/doubleo/hospitalservice/domain/area/controller/AreaController.java
+++ src/main/java/com/doubleo/hospitalservice/domain/area/controller/AreaController.java
@@ -5,21 +5,18 @@ import com.doubleo.hospitalservice.domain.area.service.AreaService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/areas")
+@RequestMapping("/hospitals")
 @RequiredArgsConstructor
 public class AreaController {
 
     private final AreaService areaService;
 
-    @GetMapping
+    @GetMapping("/{buildingId}/areas")
     @Operation(summary = "Get Areas by Building ID", description = "건물 ID로 구역 리스트 조회")
-    public List<AreaInfoResponse> areaListGetByBuildingId(@RequestParam Long buildingId) {
+    public List<AreaInfoResponse> areaListGetByBuildingId(@PathVariable Long buildingId) {
         return areaService.getAreaListByBuildingId(buildingId);
     }
 }
