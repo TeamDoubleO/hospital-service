c3480ff7f40e3a6e89c93de261c139ff557d2fb7
Jeongbeen Lee <ljb22060@gmail.com>
2025-05-23 15:02:29 +0900

    KW-428/feat: eureka 의존성 제거 (#29)

--- src/main/java/com/doubleo/hospitalservice/HospitalServiceApplication.java
+++ src/main/java/com/doubleo/hospitalservice/HospitalServiceApplication.java
@@ -2,10 +2,8 @@ package com.doubleo.hospitalservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HospitalServiceApplication {
 
     public static void main(String[] args) {
